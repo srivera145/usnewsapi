c36ae7fe34267517f7576aae9a866f1747532a27
SANTOS RIVERA <srivera145@gmail.com>
2023-03-01 11:34:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const PORT = process.env.PORT || 4060;
+const PORT = process.env.PORT;
 const app = express();
 
 const usnews = [
